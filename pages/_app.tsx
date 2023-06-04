9f0cda92fdff6b6cc16a35ff0b58e002a0d8e5cd
BrianJRobinson <robinson.j.brian@gmail.com>
2023-06-04 22:50:09 +1000

    rollback

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,7 @@ const archivo_black = Archivo_Black({
 
 // This is the chain your dApp will work on.
 // not picking up in Prod ???
-const activeChain = "polygon";
+const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
